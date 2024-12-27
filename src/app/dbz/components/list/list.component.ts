85becd531aec74319bf1029198e0a4b38a8b2ff6
maza199510 <reyesmazariego9@gmail.com>
2024-12-27 13:55:10 -0600

    Seccion dragon ball z

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: `./list.component.html`,
+  styleUrl: './list.component.css'
+})
+export class DBZListComponent {
+  @Input()
+  public characterList: Character[] = [];
+
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?:string): void{
+    if(!id) return;
+    this.onDeleteId.emit(id);
+  }
+}
